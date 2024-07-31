1957491ff7bf12ef0c61707543e72afb691fb10a
premchandrathod40 <premchandrathod40@gmail.com>
2024-07-31 07:52:22 +0530

    javascript-variabal
    javascript-variabal

---
+++ Assignment01.js
@@ -0,0 +1,5 @@
+console.log("My City name:Jalgaon");
+console.log("My best friend name:swapnil");
+console.log("My country name:India");
+console.log("My School name:Navjeen highschool,Pachora");
+console.log("My College name:SD College ,soygoan");
---
+++ test.js
@@ -0,0 +1,10 @@
+console.log("Prem Rathod");
+console.log("React Devloper");
+//variable declaration
+var firstname;
+// variable initialization
+firstname="Prem";
+console.log(firstname);
+//variable update
+firstname="Prem Rathod";
+console.log(firstname);
